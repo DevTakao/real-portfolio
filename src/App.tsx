6f74c83a97e11afdcd25cae78a1e230139d9de4f
TK <developer.aungkaungkhant@gmail.com>
2024-03-02 00:30:27 +0630

    chore: font setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <h1 className="font-title">Title</h1>
+      <p className="font-body">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui pariatur quas explicabo placeat soluta quae
+        temporibus tenetur, odit, repudiandae aperiam laudantium provident ratione molestiae. Deserunt voluptas officia
+        amet itaque ullam!
+      </p>
+    </>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  assetsInclude: ["./**/*.ttf"],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [react()],
-})
+});
