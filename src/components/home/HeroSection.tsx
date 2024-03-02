805d64d1efb91a5467ae5740e61b48075291efaf
TK <developer.aungkaungkhant@gmail.com>
2024-03-03 00:28:57 +0630

    style: hero hover effect

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -7,19 +7,29 @@ const HeroSection = () => {
       <img src={Forest} alt="forest" className="block w-full h-screen object-cover" />
 
       <div className="hero-content absolute z-[1] top-0 left-0 w-full h-full flex flex-col items-center justify-center">
-        <div className="hero-title inline-block relative">
-          <h1
-            style={{ backgroundImage: `url(${Forest})`, backgroundClip: "text" }}
-            className="font-title leading-none text-[18vw] text-white hover:[color:transparent] transition-colors duration-1000 bg-cover bg-center px-4 relative z-[1] cursor-pointer"
-          >
-            Takao
-          </h1>
-          <h1 className="font-title leading-none text-[18vw] [text-shadow:0px_-5px_10px_rgba(0,0,0,1)] [color:transparent] px-4 absolute z-0 top-0 left-0">
-            Takao
-          </h1>
-          <h2 className="text-[5vw] font-title [text-shadow:0px_-5px_10px_rgba(0,0,0,1)] px-4">
-            A Master Web Developer
-          </h2>
+        <div className="group text-center">
+          <div className="hero-title inline-block relative">
+            <h1
+              style={{ backgroundImage: `url(${Forest})`, backgroundClip: "text" }}
+              className="font-title leading-none text-[18vw] text-white group-hover:[color:transparent] transition-colors duration-1000 bg-cover bg-top px-4 relative z-[1] cursor-pointer"
+            >
+              Takao
+            </h1>
+            <h1 className="font-title leading-none text-[18vw] [text-shadow:0px_-5px_10px_rgba(0,0,0,1)] [color:transparent] px-4 absolute z-0 top-0 left-0">
+              Takao
+            </h1>
+          </div>
+          <div className="hero-title inline-block relative">
+            <h2
+              style={{ backgroundImage: `url(${Forest})`, backgroundClip: "text" }}
+              className="font-title leading-none text-[5vw] text-white group-hover:[color:transparent] transition-colors duration-1000 bg-cover bg-center px-4 relative z-[1] cursor-pointer"
+            >
+              A Master Web Developer
+            </h2>
+            <h2 className="text-[5vw] leading-none font-title [text-shadow:0px_-5px_10px_rgba(0,0,0,1)] [color:transparent] px-4 absolute z-0 top-0 left-0">
+              A Master Web Developer
+            </h2>
+          </div>
         </div>
       </div>
 
