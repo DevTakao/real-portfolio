25b6a4fad10b171a11c5571bff11af2731228671
TK <developer.aungkaungkhant@gmail.com>
2024-03-02 20:31:20 +0630

    feat: hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import ForestJPG from "@/assets/images/forest.jpg";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <section className="bg-green-dark text-white py-6 px-3">
-        <h1 className="font-title text-2xl mb-5">Hello there</h1>
-        <p className="font-body mb-5">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui pariatur quas explicabo placeat soluta quae
-          temporibus tenetur, odit, repudiandae aperiam laudantium provident ratione molestiae. Deserunt voluptas
-          officia amet itaque ullam!
-        </p>
-        <img src={ForestJPG} />
-      </section>
+      <Home />
     </>
   );
 }
---
+++ src/components/home/HeroSection.tsx
@@ -0,0 +1,34 @@
+import Forest from "@/assets/images/forest.jpg";
+import ForestOverlay from "@/assets/images/forest-overlay.png";
+
+const HeroSection = () => {
+  return (
+    <section className="hero-section relative">
+      <img src={Forest} alt="forest" className="block w-full h-screen object-cover" />
+      <div className="hero-content absolute z-[1] top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+        <div className="hero-title inline-block relative">
+          <h1
+            style={{ backgroundImage: `url(${Forest})`, backgroundClip: "text" }}
+            className="font-title leading-none text-[18vw] text-white hover:[color:transparent] transition-colors duration-1000 bg-cover bg-center px-4 relative z-[1] cursor-pointer"
+          >
+            Takao
+          </h1>
+          <h1 className="font-title leading-none text-[18vw] [text-shadow:0px_-5px_10px_rgba(0,0,0,1)] [color:transparent] px-4 absolute z-0 top-0 left-0">
+            Takao
+          </h1>
+          <h2 className="text-[5vw] font-title [text-shadow:0px_-5px_10px_rgba(0,0,0,1)] px-4">
+            A Master Web Developer
+          </h2>
+        </div>
+      </div>
+      <img
+        src={ForestOverlay}
+        alt="forest"
+        className="overlay-image block w-full h-screen object-cover absolute z-[2] top-0 left-0 pointer-events-none"
+      />
+      <div className="mask-overlay absolute z-[3] top-0 left-0 w-full h-full bg-green-dark [mask-image:radial-gradient(transparent_50%,black_80%)] pointer-events-none" />
+    </section>
+  );
+};
+
+export default HeroSection;
