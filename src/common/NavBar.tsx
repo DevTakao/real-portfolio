53ce0fe1eac7952c5cd71b1b7b07651229bd459d
TK <developer.aungkaungkhant@gmail.com>
2024-03-03 23:55:10 +0630

    feat: navbar shadow and bg on scroll

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -1,7 +1,24 @@
+import classNames from "classnames";
+import { motion, useMotionValueEvent, useScroll, useTransform } from "framer-motion";
+import { useState } from "react";
+
 const NavBar = () => {
+  const [isShadowOn, setIsShadowOn] = useState(false);
+  const { scrollY } = useScroll();
+  const opacity = useTransform(scrollY, [500, 100], [0.9, 0], { clamp: true });
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setIsShadowOn(latest >= 100);
+  });
+
   return (
-    <nav className="fixed z-[9] top-0 left-0 w-full flex items-center justify-between px-10 pt-5 pb-2 text-[3vw] md:text-lg text-white font-bold font-body [text-shadow:0px_-5px_10px_rgba(0,0,0,1)]">
-      <ul className="nav-links flex items-center justify-between w-full md:max-w-md">
+    <nav
+      className={classNames(
+        "fixed z-[9] top-0 left-0 w-full flex items-center justify-between px-10 py-3 text-[3vw] md:text-lg text-white font-bold font-body [text-shadow:0px_-5px_10px_rgba(0,0,0,1)]",
+        isShadowOn ? "shadow-lg" : "shadow-none"
+      )}
+    >
+      <motion.div style={{ opacity: opacity }} className="absolute z-[0] top-0 left-0 w-full h-full bg-green-dark" />
+      <ul className="relative z-[1] nav-links flex items-center justify-between w-full md:max-w-md">
         <li className="nav-link">
           <a href="#skills" className="font-medium hover:underline">
             Skills
@@ -25,7 +42,7 @@ const NavBar = () => {
       </ul>
       <a
         href="#contact"
-        className="hidden md:inline-block border-2 border-white rounded-full py-[2vw] px-[6vw] md:py-2 md:px-6 hover:bg-white hover:text-green-dark hover:[text-shadow:none] transition-colors duration-500"
+        className="relative z-[1] hidden md:inline-block border-2 border-white rounded-full py-[2vw] px-[6vw] md:py-2 md:px-6 hover:bg-white hover:text-green-dark hover:[text-shadow:none] transition-colors duration-500"
       >
         Contact
       </a>
