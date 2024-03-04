1e30709f45c32e7c85e63bdd45c7c8735b7b137f
TK <developer.aungkaungkhant@gmail.com>
2024-03-05 01:10:20 +0630

    feat: bush png in footer

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,13 +1,15 @@
 import { FaLinkedin, FaGithubSquare, FaFacebookSquare } from "react-icons/fa";
+import Bush from "@/assets/images/bush.png";
 
 const Footer = () => {
   return (
     <footer
       id="contact"
-      className="Footer bg-green-dark pt-[10vh] pb-[5vh] px-6 text-base font-body bg-light text-center relative 
+      className="Footer bg-green-dark pt-[10vh] pb-[10vh] sm:pb-[5vh] px-6 text-base font-body bg-light text-center relative 
     before:content-[''] before:w-full before:h-[5vh] before:bg-gradient-to-b before:from-[transparent] before:to-green-dark before:absolute before:z-[2] before:bottom-[100%] before:left-0 
     "
     >
+      <img src={Bush} className="absolute max-w-[25vw] z-[2] object-contain bottom-0 left-0 pointer-events-none" />
       <div className="socials max-w-md mx-auto text-3xl md:text-[3vw] flex items-center justify-center mb-4 [&>a]:mr-2 [&>a:last]:mr-0">
         <a
           href="https://www.linkedin.com/in/takao21/"
