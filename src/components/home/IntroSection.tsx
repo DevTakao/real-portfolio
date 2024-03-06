024e2428d5888c79070bf6b978db67b94d622696
TK <developer.aungkaungkhant@gmail.com>
2024-03-07 00:42:48 +0630

    feat: quote icon scroll anim

--- src/components/home/IntroSection.tsx
+++ src/components/home/IntroSection.tsx
@@ -1,4 +1,21 @@
 import { BiSolidQuoteAltRight } from "react-icons/bi";
+import { motion } from "framer-motion";
+
+const iconVars = {
+  initial: {
+    opacity: 0,
+    y: -25,
+  },
+  animate: {
+    opacity: 0.1,
+    y: 0,
+    transition: {
+      duration: 1.5,
+      ease: "easeOut",
+      delay: 0.1,
+    },
+  },
+};
 
 const IntroSection = () => {
   return (
@@ -9,8 +26,16 @@ const IntroSection = () => {
     >
       <div className="max-w-screen-md mx-auto px-5 relative z-[1]">
         <blockquote className="font-body text-sm md:text-[2vw] leading-normal relative">
-          <span>
-            <BiSolidQuoteAltRight className="absolute -top-7 -right-4 md:-top-[5vw] md:-right-[5vw] text-7xl md:text-[10vw] opacity-10" />
+          <span className="absolute -top-7 -right-4 md:-top-[5vw] md:-right-[5vw] text-7xl md:text-[10vw]">
+            <motion.span
+              variants={iconVars}
+              initial="initial"
+              whileInView="animate"
+              viewport={{ once: false }}
+              className="block"
+            >
+              <BiSolidQuoteAltRight />
+            </motion.span>
           </span>
           It is important to draw wisdom from many different places. If we take it from only one place, it becomes rigid
           and stale.
