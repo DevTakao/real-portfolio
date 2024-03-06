71bea3e0cd09149fe3caf2a3976a5bd94810d24b
TK <developer.aungkaungkhant@gmail.com>
2024-03-07 00:53:46 +0630

    refactor: add image alt and meta head

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -9,7 +9,11 @@ const Footer = () => {
     before:content-[''] before:w-full before:h-[5vh] before:bg-gradient-to-b before:from-[transparent] before:to-green-dark before:absolute before:z-[2] before:bottom-[100%] before:left-0 
     "
     >
-      <img src={Bush} className="absolute max-w-[25vw] z-[2] object-contain bottom-0 left-0 pointer-events-none" />
+      <img
+        src={Bush}
+        alt="bush"
+        className="absolute max-w-[25vw] z-[2] object-contain bottom-0 left-0 pointer-events-none"
+      />
       <div className="socials max-w-md mx-auto text-3xl md:text-[3vw] flex items-center justify-center mb-4 [&>a]:mr-2 [&>a:last]:mr-0">
         <a
           href="https://www.linkedin.com/in/takao21/"
