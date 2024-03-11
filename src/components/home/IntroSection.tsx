7e5badec3898fc85b9c0d92f92c5513e198ba3d2
TK <developer.aungkaungkhant@gmail.com>
2024-03-11 21:04:20 +0630

    style: intro anim

--- src/components/home/IntroSection.tsx
+++ src/components/home/IntroSection.tsx
@@ -1,6 +1,32 @@
 import { BiSolidQuoteAltRight } from "react-icons/bi";
 import { motion } from "framer-motion";
 
+const quoteWrapperVars = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 2,
+    },
+  },
+};
+
+const quoteVars = {
+  initial: {
+    opacity: 0,
+    y: 25,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 2,
+    },
+  },
+};
+
 const iconVars = {
   initial: {
     opacity: 0,
@@ -27,19 +53,24 @@ const IntroSection = () => {
       <div className="max-w-screen-md mx-auto px-5 relative z-[1]">
         <blockquote className="font-body text-sm md:text-[2vw] leading-normal relative">
           <span className="absolute -top-7 -right-4 md:-top-[5vw] md:-right-[5vw] text-7xl md:text-[10vw]">
-            <motion.span
-              variants={iconVars}
-              initial="initial"
-              whileInView="animate"
-              viewport={{ once: false }}
-              className="block"
-            >
+            <motion.span variants={iconVars} initial="initial" whileInView="animate" className="block">
               <BiSolidQuoteAltRight />
             </motion.span>
           </span>
-          It is important to draw wisdom from many different places. If we take it from only one place, it becomes rigid
-          and stale.
-          <p className="font-semibold mt-4">&mdash; General Iroh</p>
+          <motion.p
+            variants={quoteWrapperVars}
+            initial="initial"
+            whileInView="animate"
+            style={{ perspective: "600px" }}
+          >
+            <motion.span variants={quoteVars} className="block origin-bottom">
+              It is important to draw wisdom from many different places. If we take it from only one place, it becomes
+              rigid and stale.
+            </motion.span>
+            <motion.span variants={quoteVars} className="block origin-bottom font-semibold mt-4">
+              &mdash; General Iroh
+            </motion.span>
+          </motion.p>
         </blockquote>
       </div>
     </section>
