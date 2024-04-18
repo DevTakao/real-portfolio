57cc019d729cd2750ad4253d0192e59daa1d6c56
TK <developer.aungkaungkhant@gmail.com>
2024-04-18 23:20:12 +0630

    fix: vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Footer from "@/common/Footer";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import MobileNav from "./common/MobileNav";
 import DesktopNav from "@/common/DesktopNav";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}>
+      {import.meta.env.MODE === "production" && <Analytics />}
       <MobileNav />
       <DesktopNav />
       <Home />
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { inject } from "@vercel/analytics";
-
-import.meta.env.MODE === "production" && inject();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
