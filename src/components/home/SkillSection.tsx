63a4c82e2b123a4ce933b46e003e1aef189a6e25
TK <developer.aungkaungkhant@gmail.com>
2024-06-10 04:44:32 +0630

    feat: revamp skills

--- src/components/home/SkillSection.tsx
+++ src/components/home/SkillSection.tsx
@@ -1,49 +1,6 @@
 import Leaves from "@/assets/images/leaves.avif";
-import { CSSProperties, ReactNode } from "react";
-import { BiLogoJavascript, BiLogoTypescript } from "react-icons/bi";
-import { FaCss3Alt, FaHtml5, FaReact } from "react-icons/fa";
-import { TbBrandNextjs } from "react-icons/tb";
-import { motion } from "framer-motion";
+import { CSSProperties } from "react";
 import LeafIcon from "@/assets/images/leaf-icon.svg";
-
-const skillContainerVars = {
-  initial: {
-    opacity: 0,
-  },
-  animate: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.5,
-    },
-  },
-};
-
-const skillBlockVars = {
-  initial: {
-    opacity: 0,
-  },
-  animate: {
-    opacity: 1,
-    transition: {
-      duration: 1.5,
-    },
-  },
-};
-
-export type SkillBlockProps = {
-  label: string;
-  children: ReactNode;
-};
-const SkillBlock = ({ label, children }: SkillBlockProps) => {
-  return (
-    <motion.div variants={skillBlockVars} className="skill-block w-full flex flex-col items-center mx-3 mb-5">
-      <div className="text-[25vw] md:text-8xl flex items-center">{children}</div>
-      <div className="flex items-center">
-        <p className="font-body font-bold">{label}</p>
-      </div>
-    </motion.div>
-  );
-};
 
 const SkillSection = () => {
   return (
@@ -52,41 +9,183 @@ const SkillSection = () => {
         style={{ backgroundImage: `url(${Leaves})` }}
         className="absolute z-[0] w-full inset-y-0 bg-center bg-cover opacity-20"
       />
-      <div className="relative z-[1] px-5 md:px-10 max-w-screen-md mx-auto">
+      <div className="relative z-[1] px-5 md:px-10 max-w-screen-lg mx-auto">
         <h3
           style={{ "--bg-leaf": `url(${LeafIcon})` } as CSSProperties}
           className="text-2xl md:text-[3vw] font-title mb-14 text-center"
         >
           Core Skills
         </h3>
-        <motion.div
-          variants={skillContainerVars}
-          initial="initial"
-          whileInView="animate"
-          viewport={{ once: true }}
-          className="flex [&>*]:w-1/4 flex-wrap justify-center"
-        >
-          <SkillBlock label="HTML">
-            <FaHtml5 />
-          </SkillBlock>
-          <SkillBlock label="CSS">
-            <FaCss3Alt />
-          </SkillBlock>
-          <SkillBlock label="JavaScript">
-            <BiLogoJavascript />
-          </SkillBlock>
-          <SkillBlock label="TypeScript">
-            <BiLogoTypescript />
-          </SkillBlock>
-          <SkillBlock label="React.js">
-            <FaReact />
-          </SkillBlock>
-          <SkillBlock label="Next.js">
-            <TbBrandNextjs />
-          </SkillBlock>
-        </motion.div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <TypeScriptSkill />
+          <ReactJSSkill />
+          <NextJSSkill />
+          <SkillCard
+            title="Zustand"
+            description="Lightweight state management library for React, inspired by the core principles of Redux."
+          />
+          <SkillCard
+            title="Tailwind"
+            description="A class-based CSS library that makes HTML styling much easier and faster."
+          />
+          <SkillCard
+            title="Framer Motion"
+            description="Library that provides utility hooks and wrapper components for smoother animation."
+          />
+          <SkillCard title="Vite" description="Modern code bundling and development tool tailored for web projects." />
+          <SkillCard
+            title="Webpack"
+            description="Very popular code bundling tool with a lot of useful features and customization."
+          />
+          <SkillCard
+            title="Node.js"
+            description="JavaScript runtime to run development environments outside the browser. Usually used for servers."
+          />
+          <SkillCard title="Express.js" description="Library to develop secure APIs more easily with Node.js" />
+          <SkillCard
+            title="Mongodb"
+            description="NoSQL database with document-based approach. Popular with small to medium sized projects."
+          />
+          <EndCard />
+        </div>
       </div>
     </section>
+  );
+};
+
+const TypeScriptSkill = () => {
+  return (
+    <div className="flex-1 font-body inline-flex flex-col p-5 items-center border border-white/25 hover:border-white/100 transition-all duration-300 rounded-md group">
+      <div className="w-full h-full mb-5 p-[10%] max-w-[300px] relative">
+        <svg
+          className="w-full h-full
+         group-hover:animate-show-to-hide delay-500"
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              className="fill-white"
+              d="M0 16v16h32v-32h-32zM25.786 14.724c0.813 0.203 1.432 0.568 2.005 1.156 0.292 0.312 0.729 0.885 0.766 1.026 0.010 0.042-1.38 0.974-2.224 1.495-0.031 0.021-0.156-0.109-0.292-0.313-0.411-0.599-0.844-0.859-1.505-0.906-0.969-0.063-1.594 0.443-1.589 1.292-0.005 0.208 0.042 0.417 0.135 0.599 0.214 0.443 0.615 0.708 1.854 1.245 2.292 0.984 3.271 1.635 3.88 2.557 0.682 1.031 0.833 2.677 0.375 3.906-0.51 1.328-1.771 2.234-3.542 2.531-0.547 0.099-1.849 0.083-2.438-0.026-1.286-0.229-2.505-0.865-3.255-1.698-0.297-0.323-0.87-1.172-0.833-1.229 0.016-0.021 0.146-0.104 0.292-0.188s0.682-0.396 1.188-0.688l0.922-0.536 0.193 0.286c0.271 0.411 0.859 0.974 1.214 1.161 1.021 0.542 2.422 0.464 3.115-0.156 0.281-0.234 0.438-0.594 0.417-0.958 0-0.37-0.047-0.536-0.24-0.813-0.25-0.354-0.755-0.656-2.198-1.281-1.651-0.714-2.365-1.151-3.010-1.854-0.406-0.464-0.708-1.010-0.88-1.599-0.12-0.453-0.151-1.589-0.057-2.042 0.339-1.599 1.547-2.708 3.281-3.036 0.563-0.109 1.875-0.068 2.427 0.068zM18.276 16.063l0.010 1.307h-4.167v11.839h-2.948v-11.839h-4.161v-1.281c0-0.714 0.016-1.307 0.036-1.323 0.016-0.021 2.547-0.031 5.62-0.026l5.594 0.016z"
+            />
+          </g>
+        </svg>
+        <svg
+          viewBox="0 0 20 20"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          className="absolute inset-[10%] group-hover:animate-hide-to-show delay-500"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <desc>Created with Sketch.</desc> <defs> </defs>
+            <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+              <g
+                id="Dribbble-Light-Preview"
+                transform="translate(-420.000000, -7479.000000)"
+                className="fill-white opacity-0 group-hover:opacity-100 transition-opacity duration-[0.5s]"
+              >
+                <g id="icons" transform="translate(56.000000, 160.000000)">
+                  <path
+                    d="M379.328,7337.432 C377.583,7337.432 376.455,7336.6 375.905,7335.512 L375.905,7335.512 L377.435,7334.626 C377.838,7335.284 378.361,7335.767 379.288,7335.767 C380.066,7335.767 380.563,7335.378 380.563,7334.841 C380.563,7334.033 379.485,7333.717 378.724,7333.391 C377.368,7332.814 376.468,7332.089 376.468,7330.558 C376.468,7329.149 377.542,7328.075 379.221,7328.075 C380.415,7328.075 381.275,7328.491 381.892,7329.578 L380.429,7330.518 C380.107,7329.941 379.758,7329.713 379.221,7329.713 C378.67,7329.713 378.321,7330.062 378.321,7330.518 C378.321,7331.082 378.67,7331.31 379.476,7331.659 C381.165,7332.383 382.443,7332.952 382.443,7334.814 C382.443,7336.506 381.114,7337.432 379.328,7337.432 L379.328,7337.432 Z M375,7334.599 C375,7336.546 373.801,7337.575 372.136,7337.575 C370.632,7337.575 369.731,7337 369.288,7336 L369.273,7336 L369.266,7336 L369.262,7336 L370.791,7334.931 C371.086,7335.454 371.352,7335.825 371.996,7335.825 C372.614,7335.825 373,7335.512 373,7334.573 L373,7328 L375,7328 L375,7334.599 Z M364,7339 L384,7339 L384,7319 L364,7319 L364,7339 Z"
+                    id="javascript-[#155]"
+                  ></path>
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+      <div className="text-left">
+        <h3 className="font-semibold text-base mb-1 uppercase">TypeScript / JavaScript</h3>
+        <p className="text-xs">
+          Type safety added to JavaScript. Widely used by large-scale projects for scalability and maintainability.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const ReactJSSkill = () => {
+  return (
+    <div className="flex-1 font-body inline-flex flex-col p-5 items-center border border-white/25 hover:border-white/100 transition-all duration-300 rounded-md group">
+      <div className="w-full h-full mb-5 p-[10%] max-w-[300px]">
+        <svg
+          className="fill-white group-hover:animate-slow-spin"
+          viewBox="0 0 24 24"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <title>React icon</title>
+            <path d="M12 9.861A2.139 2.139 0 1 0 12 14.139 2.139 2.139 0 1 0 12 9.861zM6.008 16.255l-.472-.12C2.018 15.246 0 13.737 0 11.996s2.018-3.25 5.536-4.139l.472-.119.133.468a23.53 23.53 0 0 0 1.363 3.578l.101.213-.101.213a23.307 23.307 0 0 0-1.363 3.578l-.133.467zM5.317 8.95c-2.674.751-4.315 1.9-4.315 3.046 0 1.145 1.641 2.294 4.315 3.046a24.95 24.95 0 0 1 1.182-3.046A24.752 24.752 0 0 1 5.317 8.95zM17.992 16.255l-.133-.469a23.357 23.357 0 0 0-1.364-3.577l-.101-.213.101-.213a23.42 23.42 0 0 0 1.364-3.578l.133-.468.473.119c3.517.889 5.535 2.398 5.535 4.14s-2.018 3.25-5.535 4.139l-.473.12zm-.491-4.259c.48 1.039.877 2.06 1.182 3.046 2.675-.752 4.315-1.901 4.315-3.046 0-1.146-1.641-2.294-4.315-3.046a24.788 24.788 0 0 1-1.182 3.046zM5.31 8.945l-.133-.467C4.188 4.992 4.488 2.494 6 1.622c1.483-.856 3.864.155 6.359 2.716l.34.349-.34.349a23.552 23.552 0 0 0-2.422 2.967l-.135.193-.235.02a23.657 23.657 0 0 0-3.785.61l-.472.119zm1.896-6.63c-.268 0-.505.058-.705.173-.994.573-1.17 2.565-.485 5.253a25.122 25.122 0 0 1 3.233-.501 24.847 24.847 0 0 1 2.052-2.544c-1.56-1.519-3.037-2.381-4.095-2.381zM16.795 22.677c-.001 0-.001 0 0 0-1.425 0-3.255-1.073-5.154-3.023l-.34-.349.34-.349a23.53 23.53 0 0 0 2.421-2.968l.135-.193.234-.02a23.63 23.63 0 0 0 3.787-.609l.472-.119.134.468c.987 3.484.688 5.983-.824 6.854a2.38 2.38 0 0 1-1.205.308zm-4.096-3.381c1.56 1.519 3.037 2.381 4.095 2.381h.001c.267 0 .505-.058.704-.173.994-.573 1.171-2.566.485-5.254a25.02 25.02 0 0 1-3.234.501 24.674 24.674 0 0 1-2.051 2.545zM18.69 8.945l-.472-.119a23.479 23.479 0 0 0-3.787-.61l-.234-.02-.135-.193a23.414 23.414 0 0 0-2.421-2.967l-.34-.349.34-.349C14.135 1.778 16.515.767 18 1.622c1.512.872 1.812 3.37.824 6.855l-.134.468zM14.75 7.24c1.142.104 2.227.273 3.234.501.686-2.688.509-4.68-.485-5.253-.988-.571-2.845.304-4.8 2.208A24.849 24.849 0 0 1 14.75 7.24zM7.206 22.677A2.38 2.38 0 0 1 6 22.369c-1.512-.871-1.812-3.369-.823-6.854l.132-.468.472.119c1.155.291 2.429.496 3.785.609l.235.02.134.193a23.596 23.596 0 0 0 2.422 2.968l.34.349-.34.349c-1.898 1.95-3.728 3.023-5.151 3.023zm-1.19-6.427c-.686 2.688-.509 4.681.485 5.254.987.563 2.843-.305 4.8-2.208a24.998 24.998 0 0 1-2.052-2.545 24.976 24.976 0 0 1-3.233-.501zM12 16.878c-.823 0-1.669-.036-2.516-.106l-.235-.02-.135-.193a30.388 30.388 0 0 1-1.35-2.122 30.354 30.354 0 0 1-1.166-2.228l-.1-.213.1-.213a30.3 30.3 0 0 1 1.166-2.228c.414-.716.869-1.43 1.35-2.122l.135-.193.235-.02a29.785 29.785 0 0 1 5.033 0l.234.02.134.193a30.006 30.006 0 0 1 2.517 4.35l.101.213-.101.213a29.6 29.6 0 0 1-2.517 4.35l-.134.193-.234.02c-.847.07-1.694.106-2.517.106zm-2.197-1.084c1.48.111 2.914.111 4.395 0a29.006 29.006 0 0 0 2.196-3.798 28.585 28.585 0 0 0-2.197-3.798 29.031 29.031 0 0 0-4.394 0 28.477 28.477 0 0 0-2.197 3.798 29.114 29.114 0 0 0 2.197 3.798z"></path>
+          </g>
+        </svg>
+      </div>
+      <div className="text-left">
+        <h3 className="font-semibold text-base mb-1 uppercase">React</h3>
+        <p className="text-xs">
+          Meta's open-source library for reusable interactive UI development with state-based reactivity.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const NextJSSkill = () => {
+  return (
+    <div className="flex-1 font-body inline-flex flex-col p-5 items-center border border-white/25 hover:border-white/100 transition-all duration-300 rounded-md group">
+      <div className="w-full h-full mb-5 p-[10%] max-w-[300px]">
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="group-hover:animate-breathe">
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <g clipPath="url(#clip0)">
+              <path
+                d="M11.2141 0.00645944C11.1625 0.0111515 10.9982 0.0275738 10.8504 0.039304C7.44164 0.346635 4.24868 2.18593 2.22639 5.01291C1.10029 6.58476 0.380059 8.36775 0.107918 10.2563C0.0117302 10.9156 0 11.1103 0 12.0041C0 12.898 0.0117302 13.0927 0.107918 13.7519C0.760117 18.2587 3.96716 22.0452 8.31672 23.4481C9.0956 23.6991 9.91672 23.8704 10.8504 23.9736C11.2141 24.0135 12.7859 24.0135 13.1496 23.9736C14.7613 23.7953 16.1267 23.3965 17.4733 22.7091C17.6798 22.6035 17.7196 22.5754 17.6915 22.5519C17.6727 22.5378 16.793 21.3578 15.7372 19.9314L13.8182 17.339L11.4135 13.7801C10.0903 11.8235 9.00176 10.2235 8.99238 10.2235C8.98299 10.2211 8.97361 11.8024 8.96891 13.7331C8.96188 17.1138 8.95953 17.2499 8.9173 17.3296C8.85631 17.4446 8.80938 17.4915 8.71085 17.5431C8.63578 17.5807 8.57009 17.5877 8.21584 17.5877H7.80997L7.70205 17.5197C7.63167 17.4751 7.58006 17.4164 7.54487 17.3484L7.4956 17.2428L7.50029 12.539L7.50733 7.83285L7.58006 7.74136C7.6176 7.69209 7.69736 7.62875 7.75367 7.59825C7.84985 7.55133 7.88739 7.54664 8.29325 7.54664C8.77185 7.54664 8.85161 7.5654 8.97595 7.70147C9.01114 7.73901 10.3132 9.7003 11.871 12.0628C13.4287 14.4252 15.5589 17.651 16.6053 19.2346L18.5056 22.1132L18.6018 22.0499C19.4534 21.4962 20.3543 20.7079 21.0674 19.8868C22.5853 18.1437 23.5636 16.0182 23.8921 13.7519C23.9883 13.0927 24 12.898 24 12.0041C24 11.1103 23.9883 10.9156 23.8921 10.2563C23.2399 5.74957 20.0328 1.96306 15.6833 0.560125C14.9161 0.311445 14.0997 0.140184 13.1848 0.036958C12.9595 0.0134976 11.4088 -0.0123089 11.2141 0.00645944ZM16.1267 7.26511C16.2393 7.32142 16.3308 7.42933 16.3636 7.54194C16.3824 7.60294 16.3871 8.90734 16.3824 11.8469L16.3754 16.0651L15.6317 14.9249L14.8856 13.7848V10.7185C14.8856 8.73608 14.895 7.62171 14.9091 7.56775C14.9466 7.43637 15.0287 7.33315 15.1413 7.27215C15.2375 7.22288 15.2727 7.21819 15.6411 7.21819C15.9883 7.21819 16.0493 7.22288 16.1267 7.26511Z"
+                className="fill-white"
+              ></path>
+            </g>
+            <defs>
+              <clipPath id="clip0">
+                <rect width="24" height="24" fill="white"></rect>
+              </clipPath>
+            </defs>
+          </g>
+        </svg>
+      </div>
+      <div className="text-left">
+        <h3 className="font-semibold text-base mb-1 uppercase">Next</h3>
+        <p className="text-xs">
+          Framework built on top of React with supercharged features such as App Router and Server Side Rendering.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const SkillCard = ({ title, description }: { title: string; description: string }) => {
+  return (
+    <div className="flex-1 font-body inline-flex flex-col p-5 items-start justify-center border border-white/25 hover:border-white/100 transition-all duration-300 rounded-md">
+      <div className="text-left">
+        <h3 className="font-semibold text-base mb-1 uppercase">{title}</h3>
+        <p className="text-xs font-thin">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+const EndCard = () => {
+  return (
+    <div className="flex-1 font-body inline-flex items-center justify-center p-5 border border-white/25 hover:border-white/100 transition-all duration-300 rounded-md">
+      <p className="text-lg">... And many more.</p>
+    </div>
   );
 };
 
